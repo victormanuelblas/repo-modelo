b94e969fe4e1799124f7ec810b1617b6d1b87b09
victormanuelblas <vmblas@gmail.com>
2024-03-18 18:37:55 -0500

    fix-comp-inicio

--- src/components/inicio/index.js
+++ src/components/inicio/index.js
@@ -7,6 +7,9 @@ function Inicio() {
         <h1>Inicio</h1>
         <h2>principios</h2>
         <h3>principio 1</h3>
+        <p>
+        whatsmydns.net is a free online tool that lets you quickly and easily perform a DNS lookup to check DNS propagation and see information of any domain from DNS servers located in many countries all around the world.
+        </p>
     </div>
     
 
