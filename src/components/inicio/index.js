7cfa653d3e938969ced29b5573472daa7fd4c08f
victormanuelblas <vmblas@gmail.com>
2024-03-18 18:29:10 -0500

    initial-comit

---
+++ src/components/inicio/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Inicio() {
+  return (
+
+    <div>
+        <h1>Inicio</h1>
+        <h2>principios</h2>
+        <h3>principio 1</h3>
+    </div>
+    
+
+  )
+}
+
+export default Inicio
